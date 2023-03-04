0db45d992f33c999e0af67d523e5d81c0e2798ff
sefhuk <ish0411@naver.com>
2023-03-04 14:18:04 +0900

    Feat: (webview) 캠퍼스 건물별 좌표에 건물 이미지 마커 표시 기능 구현

---
+++ webview/src/assets/buildings.js
@@ -0,0 +1,79 @@
+export const building = [
+  {
+    engName: 'libralArts',
+    latitude: '37.213439',
+    longitude: '126.9790627',
+  },
+  {
+    engName: 'engineering',
+    latitude: '37.2100687',
+    longitude: '126.9795486',
+  },
+  {
+    engName: 'engineering1',
+    latitude: '37.209063',
+    longitude: '126.979579',
+  },
+  {
+    engName: 'engineering2',
+    latitude: '37.208625',
+    longitude: '126.979571',
+  },
+  {
+    engName: 'engineering3',
+    latitude: '37.208151',
+    longitude: '126.979971',
+  },
+  {
+    engName: 'engineering4',
+    latitude: '37.207178',
+    longitude: '126.979644',
+  },
+  { engName: 'law', latitude: '37.209227', longitude: '126.972913' },
+  {
+    engName: 'healthScience',
+    latitude: '37.209377',
+    longitude: '126.974685',
+  },
+  {
+    engName: 'fineArts',
+    latitude: '37.2080372',
+    longitude: '126.9777076',
+  },
+  {
+    engName: 'performingArts',
+    latitude: '37.21139',
+    longitude: '126.972827',
+  },
+  {
+    engName: 'centralLibrary',
+    latitude: '37.20859',
+    longitude: '126.97582',
+  },
+  { engName: 'ict', latitude: '37.210035', longitude: '126.975247' },
+  { engName: 'belkanto', latitude: '37.210376', longitude: '126.975216' },
+  { engName: 'music', latitude: '37.210354', longitude: '126.972595' },
+  {
+    engName: 'dormitory',
+    latitude: '37.210831',
+    longitude: '126.971045',
+  },
+  { engName: 'rotc', latitude: '37.206424', longitude: '126.975059' },
+  {
+    engName: 'pe',
+    latitude: '37.21211404982285',
+    longitude: '126.97906804489179',
+  },
+  { engName: 'student', latitude: '37.209124', longitude: '126.977934' },
+  { engName: 'main', latitude: '37.206708', longitude: '126.976564' },
+  { engName: 'mirae', latitude: '37.211159', longitude: '126.980017' },
+  { engName: 'ace', latitude: '37.2091062', longitude: '126.9786461' },
+  { engName: 'modeling', latitude: '37.207316', longitude: '126.978275' },
+  { engName: 'cheomdan', latitude: '37.209575', longitude: '126.979609' },
+  { engName: 'economy', latitude: '37.211759', longitude: '126.970985' },
+  {
+    engName: 'international',
+    latitude: '37.206181',
+    longitude: '126.976151',
+  },
+];
--- webview/src/components/KakaoMap.jsx
+++ webview/src/components/KakaoMap.jsx
@@ -1,12 +1,26 @@
-import { Map } from 'react-kakao-maps-sdk';
+import { Map, MapMarker } from 'react-kakao-maps-sdk';
 import styles from './KakaoMap.module.css';
+import { building } from '../assets/buildings';
 
 const KakaoMap = () => {
   return (
     <Map
       className={styles.container}
       center={{ lat: 37.20879323125512, lng: 126.97666944064247 }}
-    />
+    >
+      {building.map(e => (
+        <MapMarker
+          position={{ lat: e.latitude, lng: e.longitude }}
+          image={{
+            src: require(`../assets/buildings/${e.engName}.png`),
+            size: {
+              width: 40,
+              height: 30,
+            },
+          }}
+        />
+      ))}
+    </Map>
   );
 };
 
