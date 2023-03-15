24b37f5a29fd011694e1ef51368fe7469126d554
sefhuk <ish0411@naver.com>
2023-03-15 10:08:34 +0900

    Feat: (client) 위치제공 동의 요청 기능, 사용자 실시간 위치 트래킹 기능 구현

--- client/App.js
+++ client/App.js
@@ -1,9 +1,7 @@
-import { useState, useEffect } from 'react';
-import { useColorScheme } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import SplashScreen from './src/screens/SplashScreen';
-import MainScreen from './src/screens/Main/MainScreen';
+import Main from './src/screens/Main/Main';
 import { StatusBar } from 'expo-status-bar';
 
 const Stack = createNativeStackNavigator();
@@ -16,7 +14,7 @@ const App = () => {
         initialRouteName='Splash'
         screenOptions={{ headerShown: false }}
       >
-        <Stack.Screen name='Main' component={MainScreen} />
+        <Stack.Screen name='Main' component={Main} />
         <Stack.Screen name='Splash' component={SplashScreen} />
       </Stack.Navigator>
     </NavigationContainer>
--- client/src/screens/Main/MajorBuilding.js
+++ client/src/screens/Main/MajorBuilding.js
@@ -1,10 +1,57 @@
-import { StyleSheet, View } from 'react-native';
+import { useState, useEffect, useRef } from 'react';
+import { StyleSheet, View, Alert, Linking, AppState } from 'react-native';
 import WebView from 'react-native-webview';
+import * as Location from 'expo-location';
 
 const MajorBuilding = () => {
+  const [isPermit, setIsPermit] = useState(false);
+  const WebViewRef = useRef();
+
+  const askPermission = async () => {
+    const { granted } = await Location.getForegroundPermissionsAsync();
+    if (!granted) {
+      Alert.alert('본 앱은 위치제공 동의가 필요합니다', '', [
+        {
+          text: '확인',
+          onPress: async () => {
+            await Location.requestForegroundPermissionsAsync();
+          },
+        },
+      ]);
+    }
+    // 위치제공 동의 가정
+    setIsPermit(true);
+    trackingPosition();
+  };
+
+  const trackingPosition = async () => {
+    await Location.watchPositionAsync(
+      {
+        accuracy: Location.Accuracy.Balanced,
+        distanceInterval: 0.2,
+      },
+      position => {
+        WebViewRef.current.postMessage(
+          JSON.stringify({
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+          })
+        );
+      }
+    );
+  };
+
+  useEffect(() => {
+    askPermission();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <WebView source={{ url: 'http://hyuk.ml' }} style={styles.webview} />
+      <WebView
+        source={{ url: 'https://hyuk.ml' }}
+        style={styles.webview}
+        ref={WebViewRef}
+      />
     </View>
   );
 };
