7ce41d8157399a7c3cd2b4a59708777d3133719f
sefhuk <ish0411@naver.com>
2023-07-23 12:32:11 +0900

    Docs: (client) 탭 네비게이션 -> 스택 네비게이션으로 변경

--- client/src/navigation/MainTabs.js
+++ client/src/navigation/MainTabs.js
@@ -1,35 +1,38 @@
 import { StyleSheet, Image, View } from 'react-native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+// import {createNativeStackNavigator} from 'react-native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
 import MajorScreen from '../screens/Main/MajorScreen';
 
-const Tab = createBottomTabNavigator();
+const Tab = createNativeStackNavigator();
 
 const MainTabs = () => {
   return (
     <Tab.Navigator
       initialRouteName='Major'
       screenOptions={{
-        headerBackground: () => {
-          return (
-            <View style={styles.headerContainer}>
-              <Image source={require('../assets/image/header.png')} />
-            </View>
-          );
-        },
-        headerTitleStyle: { opacity: 0 },
-        tabBarLabel: '주요건물',
-        tabBarLabelStyle: {
-          color: '#000000',
-        },
-        tabBarIcon: () => {
-          return (
-            <View>
-              <Image
-                source={require('../assets/image/tab/majorBuilding.png')}
-              />
-            </View>
-          );
-        },
+        headerTitle: '캠퍼스',
+        headerTitleAlign: 'center',
+        // tabBarLabel: '주요건물',
+        // tabBarLabelStyle: {
+        //   color: '#000000',
+        // },
+        // headerBackground: () => {
+        //   return (
+        //     <View style={styles.headerContainer}>
+        //       <Image source={require('../assets/image/header.png')} />
+        //     </View>
+        //   );
+        // },
+        // tabBarIcon: () => {
+        //   return (
+        //     <View>
+        //       <Image
+        //         source={require('../assets/image/tab/majorBuilding.png')}
+        //       />
+        //     </View>
+        //   );
+        // },
       }}
     >
       <Tab.Screen name='Major' component={MajorScreen} />
